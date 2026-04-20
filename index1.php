f5381153162a7aef22fd75f652dbdb0ce06a2024
Фира <you@example.com>
2026-04-20 15:24:00 +0300

    создание класса прямоугольник и вывод результата

--- index1.php
+++ index1.php
@@ -25,4 +25,26 @@ class Rectangle extends Figure implements Count{
 $rectangle1 = new Rectangle(5, 5);
 echo "Площадь первого прямоугольника равна " . $rectangle1->getArea() . "см2" . "<br>";
 $rectangle2 = new Rectangle(2, 21);
-echo "Площадь второго прямоугольника равна " . $rectangle2->getArea() . "см2" ;
+echo "Площадь второго прямоугольника равна " . $rectangle2->getArea() . "см2". "<br>" ;
+
+class Triangle extends Figure implements Count{
+    private $a, $b, $c;
+    private $num = 3;
+    function __construct($a, $b, $c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+    }    
+    public function getArea(){
+        $p = ($this->a + $this->b + $this->c) / 2;
+        return sqrt($p * ($p - $this->a) * ($p - $this->b) * ($p - $this->c));
+    } 
+    public function infoAbout()
+    {
+        return "Это класс треугольника. У него " . $this->num . " стороны";
+    } 
+}
+echo "Площадь первого треугольника равна " . $triangle1->getArea() . "см2" . "<br>";
+$triangle2 = new Triangle(2, 3, 3);
+echo "Площадь второго треугольника равна " . $triangle2->getArea() . "см2" . "<br>";
